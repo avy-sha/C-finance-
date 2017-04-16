2a3dc619c38cb00fb3bf35638a53cc72d0fdbbf3
Abhinav Sharma <abhinav2061997@gmail.com>
2017-04-17 02:36:58 +0530

    Final commit

--- Project.c
+++ Project.c
@@ -12,8 +12,11 @@ by Abhinav,Sood
 #include<semaphore.h>
 #include <time.h>
 
-FILE *templog,*withdrawallog,*account,*depositlog,*transferlog;
-pthread_mutex_t mutex = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+FILE *statementlog,*templog,*withdrawallog,*account,*depositlog,*transferlog;
+pthread_mutex_t acc = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+pthread_mutex_t wd = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+pthread_mutex_t dep = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+pthread_mutex_t tra = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
 void transactions();
 int main();
 
@@ -28,9 +31,39 @@ int convert(char *a)
   return result;
 }
 
+void newaccount(){
+  account=fopen("Account","r+");
+  int balance,acctemp;
+  while(fscanf(account,"%d%d",&acctemp,&balance)!=EOF);
+  acctemp++;
+  fprintf(account,"%d %d\n",acctemp,0);
+  printf("Your account has been created with an account no: %d and initial balance -0\n",acctemp);
+  fclose(account);
+  getchar();
+  getchar();
+  return;
+}
+
+void viewaccount(int accno){
+  account=fopen("Account","r+");
+  int balance,acctemp;
+  while(fscanf(account,"%d%d",&acctemp,&balance)!=EOF)
+  {
+    if(acctemp==accno)
+    {printf("Account details are:\nAccount no:%d\nBalance:%d\n",acctemp,balance);
+      getchar();
+      getchar();
+      fclose(account);
+      main();
+    }
+  }
+
+}
+
 int accountopen(int accno)
 {
   //printf("a");
+  pthread_mutex_lock(&acc);
   account=fopen("Account","r");
   int balance,acctemp;
   while(fscanf(account,"%d%d",&acctemp,&balance)!=EOF)
@@ -41,11 +74,12 @@ int accountopen(int accno)
     }
   }
   fclose(account);
+  pthread_mutex_unlock(&acc);
   return balance;
 }
 
 void updateacc(int balance, int accno)
-{
+{ pthread_mutex_lock(&acc);
   account=fopen("Account","r+");
   int bal,acctemp,i=0,j;
   while(fscanf(account,"%d%d",&acctemp,&bal)!=EOF)
@@ -70,15 +104,16 @@ void updateacc(int balance, int accno)
     }
   }
   fclose(account);
+  pthread_mutex_unlock(&acc);
 }
 
 void *Transfer(void *t)
-{
+{ int length=0;
   int *text = (int *)t;
   time_t currtime;
   char date[100];
   //critical section starts
-  pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&tra);
   transferlog=fopen("Transferlog","a");
   int balance=accountopen(text[0]);
   int benficiarybal=accountopen(text[2]);
@@ -95,12 +130,19 @@ void *Transfer(void *t)
   {
     printf("Unsufficient Balance in account\n");
   }
+  int val=text[1];
+
+  while(val>0)
+  {
+    val=val/10;
+    length++;
+  }
   currtime=time(NULL);
   strcpy(date,ctime(&currtime));
-  fprintf(transferlog,"%ld %d %d %d %s",currtime,text[0],text[2],text[1],date);
+  fprintf(transferlog,"%ld %d %d %d %d %s",currtime,text[0],text[2],text[1],length,date);
   fclose(transferlog);
 
-  pthread_mutex_unlock(&mutex);
+  pthread_mutex_unlock(&tra);
   //critical section ends
   pthread_exit(NULL);
 }
@@ -108,10 +150,11 @@ void *Transfer(void *t)
 void *Withdrawl(void *t)
 {
   int *text = (int *)t;
+  int length=0;
   time_t currtime;
   char date[100];
   //critical section starts
-  pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&wd);
   withdrawallog=fopen("Withdrawallog","a");
   int balance=accountopen(text[0]);
 
@@ -124,41 +167,122 @@ void *Withdrawl(void *t)
   {
     printf("Unsufficient Balance in account\n");
   }
+  int val=text[1];
+
+  while(val>0)
+  {
+    val=val/10;
+    length++;
+  }
   currtime=time(NULL);
   strcpy(date,ctime(&currtime));
-  fprintf(withdrawallog,"%ld %d %d %s",currtime,text[0],text[1],date);
+  fprintf(withdrawallog,"%ld %d %d %d %s",currtime,text[0],text[1],length,date);
   fclose(withdrawallog);
 
-  pthread_mutex_unlock(&mutex);
+  pthread_mutex_unlock(&wd);
   //critical section ends
   pthread_exit(NULL);
 }
 
 void *Statement(void *t)
 {
-  int *text=(int *)t;
+  int *text=(int *)t,length=0;
+  int count=0,i=0,k=1,val,acc,acc1;
+  time_t curtime;
+  char b,c[100],d[100],e[100],f[100],g[100];
+  pthread_mutex_lock(&wd);
+  withdrawallog = fopen("Withdrawallog","r");
+  statementlog = fopen("Statementlog","w");
+  while(fscanf(withdrawallog,"%ld %d %d %d %s %s %s %s %s",&curtime,&acc,&val,&length,c,d,e,f,g)!=EOF){count=count+1;}
 
+  while(k<=count)
+  {
+
+      if(acc==text[0])
+      {
+        //fprintf(statementlog,"%ld %d %d\n",curtime,acc,val);
+        fprintf(statementlog,"%s %ld %d %d %s %s %s %s %s\n","W",curtime,acc,val,c,d,e,f,g);
+      }
+       fseek(withdrawallog,-(length+45+26),SEEK_CUR);
+       fscanf(withdrawallog,"%d",&length);
+       fseek(withdrawallog,-(length+18),SEEK_CUR);
+
+    fscanf(withdrawallog,"%ld %d %d %d %s %s %s %s %s",&curtime,&acc,&val,&length,c,d,e,f,g);
+      k++;
+  }
+  pthread_mutex_unlock(&wd);
+  count=0,k=1;
+  pthread_mutex_lock(&dep);
+  depositlog=fopen("Depositlog","r");
+  while(fscanf(depositlog,"%ld %d %d %d %s %s %s %s %s",&curtime,&acc,&val,&length,c,d,e,f,g)!=EOF){count=count+1;}
+
+  while(k<=count)
+  {
+
+      if(acc==text[0])
+      {
+        //fprintf(statementlog,"%ld %d %d\n",curtime,acc,val);
+        fprintf(statementlog,"%s %ld %d %d %s %s %s %s %s\n","D",curtime,acc,val,c,d,e,f,g);
+      }
+       fseek(depositlog,-(length+45+26),SEEK_CUR);
+       fscanf(depositlog,"%d",&length);
+       fseek(depositlog,-(length+18),SEEK_CUR);
+
+    fscanf(depositlog,"%ld %d %d %d %s %s %s %s %s",&curtime,&acc,&val,&length,c,d,e,f,g);
+      k++;
+  }
+  pthread_mutex_unlock(&dep);
+  count=0,k=1;
+  pthread_mutex_lock(&tra);
+  transferlog=fopen("Transferlog","r");
+  while(fscanf(transferlog,"%ld %d %d %d %d %s %s %s %s %s",&curtime,&acc,&acc1,&val,&length,c,d,e,f,g)!=EOF){count=count+1;};
+  while(k<=count)
+  {
+      if(acc==text[0])
+      {
+        fprintf(statementlog,"%s %ld %d %d %d %s %s %s %s %s\n","TT",curtime,acc,acc1,val,c,d,e,f,g);
+      }
+      else if(acc1==text[0])
+      {
+        fprintf(statementlog,"%s %ld %d %d %d %s %s %s %s %s\n","TF",curtime,acc,acc1,val,c,d,e,f,g);
+      }
+      fseek(transferlog,-(length+49+26),SEEK_CUR);
+      fscanf(transferlog,"%d",&length);
+      fseek(transferlog,-(length+23),SEEK_CUR);
+      fscanf(transferlog,"%ld %d %d %d %d %s %s %s %s %s",&curtime,&acc,&acc1,&val,&length,c,d,e,f,g);
+      k++;
+  }
+  fclose(statementlog);
+  pthread_mutex_unlock(&tra);
+  return NULL;
 }
 
 void *Deposit(void *t)
-{
+{ int length=0;
   int *text = (int *)t;
   time_t currtime;
   char date[100];
 
   //critical section starts
-  pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&dep);
   depositlog=fopen("Depositlog","a");
   int balance=accountopen(text[0]);
 //  printf("%d",balance);
     balance=balance+text[1];
     updateacc(balance,text[0]);
+    int val=text[1];
 
+    while(val>0)
+    {
+      val=val/10;
+      length++;
+    }
     currtime=time(NULL);
     strcpy(date,ctime(&currtime));
-    fprintf(depositlog,"%ld %d %d %s",currtime,text[0],text[1],date);
+    fprintf(depositlog,"%ld %d %d %d %s",currtime,text[0],text[1],length,date);
+    //fprintf(depositlog,"%ld %d %d",currtime,text[0],text[1]);
     fclose(depositlog);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&dep);
 
         //critical section ends
 
@@ -208,7 +332,7 @@ while(fscanf(templog,"%s %s %s %s",text1,text2,text3,text4)!=EOF){
   {
     text[0]=convert(text2);
     pthread_create(&thread[no],NULL,&Statement,(void*)text);
-    pthread_join(thread[no], NULL);
+    //pthread_join(thread[no], NULL);
   }
   else if (strcmp(text4,"-")!=0){
     text[0]=convert(text2);
@@ -287,36 +411,32 @@ system("tput clear");
 int i,j,k,l,n,m,o,p;
 //printf("%d",getpid());
 while(1){
+printf("FCFS BANKING SYSTEM\n\n\n");
 printf("Enter the option\n");
 printf("1.Create new account\n");
 printf("2.View account\n");
-printf("3.Edit details\n");
-printf("4.C banking\n");
-printf("5.Exit\n");
-//clean_stdin();
-//printf("%d",getpid());
+printf("3.C banking\n");
+printf("4.Exit\n");
 n=getchar();
-//getchar();
+int accno;
 switch (n) {
   case 49:{//clean_stdin();
           system("tput clear");
-          //newaccount();
+          newaccount();
           break;}
   case 50:{//clean_stdin();
           system("tput clear");
-          //viewaccount();
+          printf("Enter the account no whose details you want to view:");
+          scanf("%d",&accno);
+          viewaccount(accno);
           break;}
-  case 51:{//clean_stdin();
-          system("tput clear");
-          //editdetails();
-          break;}
-  case 52:{//clean_stdin();
-          system("tput clear");
+  case 51:{system("tput clear");
           banking();
           break;}
-  case 53:{printf("Exiting the program\n");
+  case 52:{printf("Exiting the program\n");
           exit(5);
-          break;}
+          break;
+          }
   default:{ //clean_stdin();
             system("tput clear");
             printf("Invalid choice\n");
